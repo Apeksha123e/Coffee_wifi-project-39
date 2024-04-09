da3d29a3300bdfe62a34a5455a2156edc1da26c9
Apeksha <apekshahampimath@gmail.com>
2024-04-09 20:53:07 +0700

    create Procfile

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
+import os
 
 '''
 Red underlines? Install the required packages first: 
@@ -19,7 +20,7 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 Bootstrap5(app)
 
 
